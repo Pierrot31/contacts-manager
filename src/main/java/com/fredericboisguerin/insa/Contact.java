0300cbb9c21ca23ddad8b851ff19158316e74ab2
Pierrot31 <plaurens@etud.insa-toulouse.fr>
2017-11-13 14:48:00 +0100

    USERINTERFACE CODE PROPRE avec toutes les classes

---
+++ src/main/java/com/fredericboisguerin/insa/Contact.java
@@ -0,0 +1,46 @@
+package com.fredericboisguerin.insa;
+
+public class Contact {
+    String name;
+    String email;
+    String phoneNumber;
+    String chaine_retour;
+
+    /**
+     * @param name
+     * @param email
+     * @param phoneNumber
+     */
+    public Contact(String name,String email, String phoneNumber){
+            this.name = name;
+            this.email = email;
+            this.phoneNumber = phoneNumber;
+    }
+    @Override
+    public String toString() {
+        if (this.email == null && this.phoneNumber != null) {
+            chaine_retour =  this.name + ", " + this.phoneNumber;
+        }
+        if (this.email != null && this.phoneNumber == null) {
+            chaine_retour =  this.name + ", " + this.email;
+        }
+        if (this.email == null && this.phoneNumber == null) {
+            chaine_retour =  this.name;
+        }
+        if (this.email != null && this.phoneNumber != null) {
+            chaine_retour =  this.name + ", " + this.email + ", " + this.phoneNumber;
+        }
+        return chaine_retour;
+    }
+
+    public void modifyName(String name){
+        this.name = name;
+    }
+    public void modifyemail(String email){
+        this.email = email;
+    }
+    public void modifyPhoneNumber(String phoneNumber){
+        this.phoneNumber = phoneNumber;
+    }
+
+}
---
+++ src/test/java/com/fredericboisguerin/insa/ContactsManagerTest.java
@@ -0,0 +1,7 @@
+package com.fredericboisguerin.insa;
+
+import static org.junit.Assert.*;
+
+public class ContactsManagerTest {
+
+}
